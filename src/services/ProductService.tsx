4652ca6106ff168d2b4678c9e7065c32f9dbf4ec
holymiko <holymiko@cvut.cz>
2023-04-21 11:12:15 +0200

    ProductController byParams

--- src/services/ProductService.tsx
+++ src/services/ProductService.tsx
@@ -6,7 +6,14 @@ export const getProductsAsDTO = (metal: string|undefined) => {
     if(metal === undefined || metal === 'all') {
         return api.get("product");
     }
-    return api.get("product/metal/"+metal);
+    return api.get(
+        "product",
+        {
+            params: {
+                metal: metal.toUpperCase()
+            }
+        }
+    );
 }
 
 export const getProductById = async (productId: number): Promise<Product> => {
