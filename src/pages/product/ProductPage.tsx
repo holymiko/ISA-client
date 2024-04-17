b8adfd1cd477279417cf052613435f3f66d5ba77
holymiko <holymiko@cvut.cz>
2024-04-17 20:41:45 +0200

    disable save separately in production

--- src/pages/product/ProductPage.tsx
+++ src/pages/product/ProductPage.tsx
@@ -299,7 +299,7 @@ export const ProductPage = () => {
                                 sortable: false,
                                 renderCell: (params: GridRenderCellParams<Product>) => (
                                     <Box sx={{gap: 2, width: 1.0, display: 'flex', justifyContent: 'center'}}>
-                                        <ButtonISA onClick={() => saveProductSeparatelyFce(params.row.id)}>
+                                        <ButtonISA disabled={process.env.REACT_APP_PROFILE === 'prod'} onClick={() => saveProductSeparatelyFce(params.row.id)}>
                                             save separately
                                         </ButtonISA>
                                     </Box>
